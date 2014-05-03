08d7d607d83cb1c497b7c9566c3b9d40be94c37f
sebabelmar <si.belmar@gmail.com>
2014-05-03 16:32:53 -0700

    1st JS with EOIN

--- week_8_and_9/3_JavaScript/grocery_list/my_solution.js
+++ week_8_and_9/3_JavaScript/grocery_list/my_solution.js
@@ -1,29 +1,103 @@
 // U3.W8-9: 
 
 
-// I worked on this challenge [by myself, with: ].
+// I worked on this challenge Eoin
 
 // 2. Pseudocode
+
+// DEFINE var ShoppingList that will hold functions inside.
+//  DEFINE var list as an empty array
+//  DEFINE  fuctions add, display, remove
+// TEST
 
 
 
 // 3. Initial Solution
 
+// var shoppingList = new function() {
+//   var list = [];
 
+//   this.addItem = function(item) {
+//    list.push(item);
+//   }
+
+//   this.displayList = function(){
+//    console.log("Your Shopping List:");
+//    console.log(list);
+//   }
+
+//   this.removeItem = function(item){
+//    for(var i = 0; i <= list.length; i++){
+//      if(item === list[i]){
+//        delete list[i];
+//      }
+//    }
+//   }  
+
+//  }
 
 
 // 4. Refactored Solution
+var list = [];
+
+var shoppingList = new function() {
+
+  this.addItem = function(item) {
+    list.push(item);
+  }
+
+  this.displayList = function(){
+    console.log("Your list!: ")
+    console.log(list);
+  }
+
+  this.removeItem = function(item){
+    for(var i = 0; i <= list.length; i++){
+      if(item === list[i]){
+        list.splice(i,1);
+      }
+    }
+  } 
 
 
-
-
-
+ }
+shoppingList.addItem("apple")
+shoppingList.addItem("banana")
+shoppingList.addItem("cherry")
+shoppingList.addItem("cheese")
+shoppingList.displayList()
+shoppingList.removeItem("banana")
+shoppingList.displayList()
 
 // 1. DRIVER TESTS/ASSERT STATEMENTS GO BELOW THIS LINE
 
-
-
-
+// We try driving test and even with help of one of our tutors was "impossible"
+// we left the program printing something 'fun'
+// shoppingList.addItem("apple")
+// console.log(list === ["apple"])
+// shoppingList.addItem("banana")
+// shoppingList.addItem("cherry")
+// shoppingList.addItem("cheese")
+// console.log(shoppingList.displayList() === [ 'apple', 'banana', 'cherry', 'cheese' ])
+// shoppingList.removeItem("banana")
+// console.log(shoppingList.displayList() === [ 'apple', 'cherry', 'cheese' ])
 
 
 // 5. Reflection 
+
+// I didn’t have time to go through the JS tutorial during the prep work, I did some
+// during week 2, and now I had to do it.
+// JS was causing me more problems than I thought.
+// I met with Eoin and we finally find the way to make this work similar as Ruby.
+// Definitely I need to spend more time on JS and JQuery.
+// I have no Idea how to an interactive shopping List out of this code.
+// So I think that's coming next.
+// On this challenge I learned about JS functions and a lot a bout how to deal with
+// array in this language. I have faith in JS :)
+
+
+
+
+
+
+
