df25a1e9b77f4763fa25eece9bfd3c12af39a810
sebabelmar <si.belmar@gmail.com>
2014-05-04 00:01:47 -0700

    sql

--- week_8_and_9/5_SQL/SQLite_gem/my_solution.rb
+++ week_8_and_9/5_SQL/SQLite_gem/my_solution.rb
@@ -1,6 +1,6 @@
 # U3.W7: BONUS Using the SQLite Gem
 
-# I worked on this challenge [by myself, with:]
+# I worked on this challenge Kevin
 
 require 'sqlite3'
 
@@ -8,20 +8,31 @@ require 'sqlite3'
 $db = SQLite3::Database.open "congress_poll_results.db"
 
 
-def print_arizona_reps
-  puts "AZ REPRESENTATIVES"
-  az_reps = $db.execute("SELECT name FROM congress_members WHERE location = 'AZ'")
-  az_reps.each { |rep| puts rep }
+def print_arizona_reps(state)
+  puts "#{state} REPRESENTATIVES"
+  reps = $db.execute("SELECT name FROM congress_members WHERE location = '#{state.to_s}'")
+  reps.each { |rep| puts rep }
 end
 
 def print_longest_serving_reps(minimum_years)  #sorry guys, oracle needs me, i didn't finish this!
   puts "LONGEST SERVING REPRESENTATIVES"
-  puts $db.execute("SELECT name FROM congress_members WHERE years_in_congress > #{minimum_years}")
+  name = $db.execute("SELECT name FROM congress_members WHERE years_in_congress > #{minimum_years}")
+  year = $db.execute("SELECT years_in_congress FROM congress_members WHERE years_in_congress > #{minimum_years}")
+  
+  name_year = name.zip(year)
+  name_year.each {|name, year| puts "#{name} - #{year} years"}
 end
 
-def print_lowest_grade_level_speakers
-  puts "LOWEST GRADE LEVEL SPEAKERS (less than < 8th grade)"
+def print_lowest_grade_level_speakers(grade_level)
+  puts "LOWEST GRADE LEVEL SPEAKERS (less than < #{grade_level})"
+  puts $db.execute("SELECT name, grade_current FROM congress_members WHERE grade_current < #{grade_level}")
 end
+
+def print_votes_by_reps 
+  puts "AMOUNTS OF VOTES PER REPRESENTATIVE"
+  puts $db.execute("SELECT name, count(politician_id) AS votes FROM congress_members JOIN votes ON (congress_members.id = politician_id) GROUP BY name")
+end
+
 
 def print_separator
   puts 
@@ -29,8 +40,18 @@ def print_separator
   puts 
 end
 
+#years_in_congress
+print_arizona_reps("AZ")
+print_separator
+print_arizona_reps("NY")
+print_separator
+print_arizona_reps("AK")
+print_separator
+print_arizona_reps("NJ")
+print_separator
+print_arizona_reps("FL")
 
-print_arizona_reps
+
 
 print_separator
 
@@ -39,7 +60,10 @@ print_longest_serving_reps(35)
 # output should look like:  Rep. C. W. Bill Young - 41 years
 
 print_separator
-print_lowest_grade_level_speakers 
+print_lowest_grade_level_speakers(8)
+
+print_separator
+print_votes_by_reps
 # TODO - Need to be able to pass the grade level as an argument, look in schema for "grade_current" column
 
 # TODO - Make a method to print the following states representatives as well:
@@ -55,15 +79,56 @@ print_lowest_grade_level_speakers
 #    have a similar format)
 # Create a listing of each Politician and the voter that voted for them
 # output should include the senators name, then a long list of voters separated by a comma
-#
+
+def voter_per_member
+  members = $db.execute("SELECT politician_id FROM votes")
+  voters = $db.execute("SELECT voter_id FROM votes")
+  mem_vot = members.zip(voter)
+  
+    
+  mem_vot.each do |k, v|
+    k = $db.execute("SELECT name FROM congress_members WHERE id = #{k}")
+    v = $db.execute("SELECT first_name FROM voters WHERE id = #{v}")
+  end
+
+  GROUP BY KEY
+  
+	t = [
+	  {nil => 1, 10 => 2, 16 => 4, 5=> 10},
+	  {nil => 9, 5 => 2, 17 => 3, 10 => 2},
+	  {10 => 4, 5 => 9, 17 => 1}
+	]
+
+	# Create hash of possible keys
+	keys = t.reduce({}) { |m, h| h.each_key { |k| m[k] = [] }; m }
+
+	# Iterate through array, for each hash, for each key, append the 
+	# value if key is in hash or zero otherwise
+	t.reduce(keys) { |m, h| m.each_key { |k| m[k] << (h[k] || 0) }; m }
+
+	puts keys
+  
+end
+
+
 # * you'll need to do some join statements to complete these last queries!
 
 
 # REFLECTION- Include your reflection as a comment below.
-# How does the sqlite3 gem work?  What is the variable `$db` holding?  
-# Try to use your knowledge of ruby and OO to decipher this as well as h
-# ow the `#execute` method works.  Take a stab at explaining the line 
+
+# How does the sqlite3 gem work?  
+# The gem passes SQL statements form ruby, execute them and return results into ruby.
+# Seems like it going down to terminal, execute statements on sqlie3 on it and save 
+# the result to bring it to ruby.
+
+# What is the variable `$db` holding?  
+# This is a universal variable holding the data base.
+  
+
+# Take a stab at explaining the line 
 # `$db.execute("SELECT name FROM congress_members WHERE years_in_congress 
 #   > #{minimum_years}")`.  Try to explain this as clearly as possible for 
 # your fellow students.  
-# If you're having trouble, find someone to pair on this explanation with you.
+# Returns one column with multiples rows of names from the table congress_members that 
+# have values for the variable years in congress > a speficy number seted by a parameter 
+# it will be passed on a method.
