d4849893531f70573b2c5873ee27897feb2ecacf
sebabelmar <si.belmar@gmail.com>
2014-05-03 20:04:31 -0700

    JS done

--- week_8_and_9/3_JavaScript/translate_to_js/my_solution.js
+++ week_8_and_9/3_JavaScript/translate_to_js/my_solution.js
@@ -9,21 +9,54 @@
 
 // 3. Initial Solution
 
+// def is_fibonacci?(num)
+// 	return true if num == 0 || num == 1
+// 	fb_pair = [2,3]
 
+// 	while fb_pair.inject(:+) < num do
+// 		fb_nxt_num = fb_pair.inject(:+)
+// 		fb_pair.shift 
+// 		fb_pair << fb_nxt_num
+// 	end
+
+// 	fb_pair[0] + fb_pair[1] == num ? true : false
+
+// end
 
 
 // 4. Refactored Solution
 
+var isFibonnaci = function(num){
+	if(num === 1 || num === 0){
+		return true
+	}
 
+	var fb_pair = [1,2]
+	while (fb_pair[0] + fb_pair[1] < num){
+		var fb_nxt_num = fb_pair[0] + fb_pair[1]
+		fb_pair.shift() 
+		fb_pair.push(fb_nxt_num) 
+	}
 
+	if((fb_pair[0] + fb_pair[1]) === num ){
+		return true
+	}
+	else{
+		return false
+	}
+
+}
 
 
 
 // 1. DRIVER TESTS/ASSERT STATEMENTS GO BELOW THIS LINE
 
-
-
-
+console.log(isFibonnaci(1) === true)
+console.log(isFibonnaci(8) === true)
+console.log(isFibonnaci(13) === true)
+console.log(isFibonnaci(40) === false)
+console.log(isFibonnaci(323) === false)
+console.log(isFibonnaci(927372692193078999171) === false)
 
 
 // 5. Reflection 
